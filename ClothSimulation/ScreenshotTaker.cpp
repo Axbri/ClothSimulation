10b91ac515fe19c3e94f747908aea558786e63c8
Axbri <axel.brinkeby@gmail.com>
2016-02-28 18:30:00 +0100

    Fixat bugg när screenshot-taker sparar bmp-filer

--- ClothSimulation/ScreenshotTaker.cpp
+++ ClothSimulation/ScreenshotTaker.cpp
@@ -14,10 +14,38 @@ void ScreenshotTaker::screenshot(int x, int y)
 	int xb = (x - xa) / 256;
 	int ya = y % 256;
 	int yb = (y - ya) / 256;
+		
+	unsigned char fileheader[14] = {
+		'B','M', // magic
+		0,0,0,0, // size in bytes
+		0,0, // app data
+		0,0, // app data
+		40 + 14,0,0,0 // start of data offset
+	};
+	unsigned char infoheader[40] = {
+		40,0,0,0, // info hd size
+		0,0,0,0, // width
+		0,0,0,0, // heigth
+		1,0, // number color planes
+		24,0, // bits per pixel
+		0,0,0,0, // compression is none
+		0,0,0,0, // image bits size
+		0x13,0x0B,0,0, // horz resoluition in pixel / m
+		0x13,0x0B,0,0, // vert resolutions (0x03C3 = 96 dpi, 0x0B13 = 72 dpi)
+		0,0,0,0, // #colors in pallete
+		0,0,0,0, // #important colors
+	};
 
-	// assemble the header
-	unsigned char header[18] = { 0,0,2,0,0,0,0,0,0,0,0,0,(char)xa,(char)xb,(char)ya,(char)yb,24,0 };
+	infoheader[4] = (unsigned char)(x);
+	infoheader[5] = (unsigned char)(x >> 8);
+	infoheader[6] = (unsigned char)(x >> 16);
+	infoheader[7] = (unsigned char)(x >> 24);
 
+	infoheader[8] = (unsigned char)(y);
+	infoheader[9] = (unsigned char)(y >> 8);
+	infoheader[10] = (unsigned char)(y >> 16);
+	infoheader[11] = (unsigned char)(y >> 24);
+	
 	// create a filemane containing the current time.
 	time_t now = time(0);
 	struct tm localtm;
@@ -32,7 +60,8 @@ void ScreenshotTaker::screenshot(int x, int y)
 	// write header and data to file
 	ofstream file; 
 	file.open(filemanebuffer, ios::out | ios::binary);
-	file.write(reinterpret_cast <char*>(header), sizeof(char) * 18);
+	file.write(reinterpret_cast <char*>(fileheader), sizeof(char) * 14);
+	file.write(reinterpret_cast <char*>(infoheader), sizeof(char) * 40);
 	file.write(reinterpret_cast <char*>(data), sizeof(char)*imageSize);
 	file.close();
 	
